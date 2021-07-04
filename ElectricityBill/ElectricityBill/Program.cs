f840875f28c90fdaad1a0257efbec0bd76576950
INAAMULLAH1414 <56350545+INAAMULLAH1414@users.noreply.github.com>
2021-07-04 12:34:29 +0500

    ALL files Uploaded

---
+++ ElectricityBill/ElectricityBill/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+
+
+namespace ElectricityBill
+{
+    class Program
+    {
+        public static Double surcharge(double currbill)
+        {
+            Double bill=0;
+            if (currbill > 400)
+            {
+                bill = currbill * 0.15;
+            }
+            Console.WriteLine("Surcharge Amount : "+bill);
+            return bill ;
+        }
+        public static void createFile()
+        {
+            Console.WriteLine("Enter file name : ");
+            string fileN = Console.ReadLine();
+            Console.WriteLine("Enter path in proper format " );
+            string p = Console.ReadLine();
+
+            string path = p + @"\"+fileN+".txt";
+            
+            if (File.Exists(path)) 
+            {
+                Console.WriteLine("file already exists");
+                return ;
+            }
+            try
+            {
+                // Create the file, or overwrite if the file exists.
+                FileStream fs = new FileStream(path, FileMode.Create);
+           
+                Console.WriteLine("file created sucessfully");
+            }
+            catch (Exception ex)  
+            {
+                Console.WriteLine("incorrect path");
+
+            }
+            
+        }
+        public static double calculateBill(int unit)
+        {
+            
+            double bill=-1;
+             if (unit <=199)
+            {
+                bill = unit * 1.20;
+                Console.WriteLine("Amount Charges @RS.: 1.2 per unit : "+ bill);
+               
+            }
+             else if(unit >= 200 && unit <400)
+            {
+                bill = unit * 1.50;
+                Console.WriteLine("Amount Charges @RS.: 1.50 per unit : " + bill);
+            }
+            else if (unit >= 400 && unit < 600)
+            {
+                bill = unit * 1.80;
+                Console.WriteLine("Amount Charges @RS.: 1.80 per unit : " + bill);
+
+            }
+            else if (unit >= 600)
+            {
+
+                bill = unit * 2.0;
+                Console.WriteLine("Amount Charges @RS.: 2.0 per unit : " + bill);
+               }
+
+            return bill;
+        }
+
+        
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int id = -1, unit = 0;
+                string name = "";
+                Console.WriteLine("Enter id :");
+                id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Name :");
+                name = Console.ReadLine();
+                Console.WriteLine("Enter unit :");
+                unit = int.Parse(Console.ReadLine());
+                Console.Clear();
+
+                Console.WriteLine("Customer Id : " + id);
+                Console.WriteLine("Customer Name : " + name);
+                Console.WriteLine("Units Consume:  " + unit);
+                Double curr = calculateBill(unit);
+                Double net = surcharge(curr);
+                Console.WriteLine("Surcharge Amount : " + net);
+                Console.WriteLine("Net amount paid by the customer : " + (net + curr));
+                //Task 3 : 
+                //saving data
+                Console.WriteLine(" '\n' Task 3 :");
+                save.save_file(id, name, unit, net, curr);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Incorrect input");
+            }
+            finally
+            {
+
+                // Task 2 :
+                Console.WriteLine("Task 2 :");
+                createFile();
+
+            }
+
+
+
+        }
+    }
+}
---
+++ ElectricityBill/ElectricityBill/save.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+namespace ElectricityBill
+{
+    public class save
+    {
+         public static void save_file(int id,String name,int unit,Double net,Double curr)
+        {
+            string str = id + "_" + name;
+            try
+            {
+                // Create the file, or overwrite if the file exists.
+
+                FileStream fs =new FileStream(str + ".txt",FileMode.Append,FileAccess.Write);
+                var sr = new StreamWriter(fs);
+                sr.WriteLine("Customer Id : " + id);
+                sr.WriteLine("Customer Name : " + name);
+                sr.WriteLine("Units Consume:  " + unit);
+                if (unit <= 199)
+                {
+                   
+                    sr.WriteLine("Amount Charges @RS.: 1.2 per unit : " + curr);
+
+                }
+                else if (unit >= 200 && unit < 400)
+                {
+                    
+                    sr.WriteLine("Amount Charges @RS.: 1.50 per unit : " + curr);
+                }
+                else if (unit >= 400 && unit < 600)
+                {
+                   
+                    sr.WriteLine("Amount Charges @RS.: 1.80 per unit : " + curr);
+
+                }
+                else if (unit >= 600)
+                { 
+        
+                    sr.WriteLine("Amount Charges @RS.: 2.0 per unit : " + curr);
+                }
+
+                sr.WriteLine("Surcharge Amount : " + net);
+                sr.WriteLine("Net amount paid by the customer : " + (net + curr));
+                sr.Close();
+                Console.WriteLine("info saved");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error");
+
+            }
+
+        }
+    }
+}
